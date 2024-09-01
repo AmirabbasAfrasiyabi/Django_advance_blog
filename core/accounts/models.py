10875d175ab02e1c9aaa1d21dd63ca035cba92ae
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-09-01 12:47:09 +0330

    work with abstrach user and abstrack base user

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser ,PermissionsMixin)
 
 # Create your models here.
+
+
+class UserManager(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(max_length=255,unique=True)
+    is_stuff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    # is_verified = models.BooleanField(default=False)
+    first_name = models.CharField(max_length=20)
+    REQUIRED_FIELDS = []
+
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    def __str__(self):
+        return self.email
