36d644ee07d6b113aadc33636a9b84b92ae06398
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-11-02 10:57:37 +0330

    created models for accounts

--- core/account/models.py
+++ core/account/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.contrib.auth.models import Group, Permission
 
-# Create your models here.
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    first_name = models.CharField(max_length=20)
+    REQUIRED_FIELDS = []
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField()
+
+    # افزودن related_name به groups و user_permissions
+    groups = models.ManyToManyField(
+        Group,
+        related_name="customuser_groups",  # تغییر نام ارتباط معکوس
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="customuser_permissions",  # تغییر نام ارتباط معکوس
+        blank=True
+    )
+
+    def __str__(self):
+        return self.email
