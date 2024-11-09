e5a0d495e735f5d2d6ef669dd8ab171134c903f3
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-11-09 15:22:29 +0330

    repaire error for show post in cbv

--- core/blog/views.py
+++ core/blog/views.py
@@ -16,4 +16,5 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["name"] = 'ali'
+        context["posts"] = Post.objects.all()  # Fetch all posts from the database and add it to the context.  # Assuming Post is a model that represents your blog posts.  # Replace this line with your actual database query.  # This is a simple example to demonstrate the concept.  # In a real-world application, you should fetch the posts based on the user's preferences and other criteria.  # Also, you should paginate the posts to avoid
         return context
