2e14efbb6f872f640dd960b1b0000d48113ac7fb
AmirabbasAfrasiyabi <aa.afrasiyabi@yahoo.com>
2024-11-17 15:30:49 +0330

    work with serializer

--- core/blog/api/V1/views.py
+++ core/blog/api/V1/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import PostSerializer
+from blog.models import Post
 
 data={
     'id':1,
@@ -12,4 +14,7 @@ def PostList(request):
 
 @api_view()
 def PostDetail(request,id):
-    return Response(data)
+    post = Post.objects.get(pk=id)
+    Serializer = PostSerializer(post)
+    print(Serializer.data)
+    return Response(Serializer.data)
